99db6527dbd320ac839995398357efa1cd150654
Frank Naw <franknaw@gmail.com>
2022-09-06 10:11:07 -0400

    Updates

--- function/run.csx
+++ function/run.csx
@@ -20,7 +20,7 @@ public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
     name = name ?? data?.name;
 
     return name != null
-        ? (ActionResult)new OkObjectResult($"Hello, {name})
+        ? (ActionResult)new OkObjectResult($"Hello Production", {name})
         : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
 }
 
--- main.tf
+++ main.tf
@@ -1,25 +1,5 @@
 /*
-Create a web app service plan.
-  An App Service plan defines a set of compute resources for a web app to run.
-  These compute resources are analogous to the server farm in conventional web hosting. 
-  One or more apps can be configured to run on the same computing resources (or in the same App Service plan).
-  See: https://docs.microsoft.com/en-us/azure/app-service/overview-hosting-plans
-*/
-resource "azurerm_service_plan" "sp" {
-  name                = "function-sp-${var.plan_os_type}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  os_type             = var.plan_os_type
-  sku_name            = var.plan_sku_name
-  tags = merge({
-    Name = "${var.product_name}-${var.plan_os_type}-service-plan",
-    },
-    var.tags
-  )
-}
-
-/*
-Create a storage account to contain the function code
+Create storage account to contain the function code
   An Azure storage account contains all of your Azure Storage data objects, including blobs, file shares, queues, tables, and disks. 
   The storage account provides a unique namespace for your Azure Storage data that's accessible from anywhere in the world over HTTP or HTTPS
   See: https://docs.microsoft.com/en-us/azure/storage/common/storage-account-overview
@@ -39,7 +19,7 @@ resource "azurerm_storage_account" "storage_account" {
 }
 
 /*
-Create a windows app function using dotnet to run a C# function
+Create windows app function using dotnet to run a C# function
   Azure Function is a serverless compute service that enables user to run event-triggered code without having to provision or manage infrastructure. 
   Being as a trigger-based service, it runs a script or piece of code in response to a variety of events.
   See: https://docs.microsoft.com/en-us/azure/azure-functions/functions-get-started?pivots=programming-language-csharp
@@ -48,7 +28,7 @@ resource "azurerm_windows_function_app" "function_app" {
   name                = var.app_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  service_plan_id     = azurerm_service_plan.sp.id
+  service_plan_id     = var.service_plan.id
 
   storage_account_name       = azurerm_storage_account.storage_account.name
   storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
@@ -59,7 +39,6 @@ resource "azurerm_windows_function_app" "function_app" {
     }
   }
 
-
   tags = merge({
     Name = "${var.product_name}-${var.plan_os_type}-app-function",
     },
@@ -69,19 +48,19 @@ resource "azurerm_windows_function_app" "function_app" {
 }
 
 /*
-Create a app function with a HTTP trigger binding
+Create an app function with a HTTP trigger binding
   A function that will be run whenever it receives an HTTP request, responding based on data in the body or query string
   See: https://docs.microsoft.com/en-us/azure/azure-functions/functions-triggers-bindings?tabs=csharp
 */
 resource "azurerm_function_app_function" "function_app_function" {
-  name            = "function-app-function-${var.app_name}"
+  name            = "function-${var.app_name}"
   function_app_id = azurerm_windows_function_app.function_app.id
   language        = "CSharp"
 
 
   file {
     name    = "run.csx"
-    content = file("../azure-simple-app-service-function/function/run.csx")
+    content = file("../../azure-simple-app-service-function/function/run.csx")
   }
 
   test_data = jsonencode({
@@ -110,3 +89,55 @@ resource "azurerm_function_app_function" "function_app_function" {
 }
 
 
+## Work in progress
+# resource "azurerm_windows_function_app_slot" "function_app_function_slot" {
+#   name            = "function-slot-${var.app_name}"
+#   function_app_id = azurerm_windows_function_app.function_app.id
+
+#   storage_account_name       = azurerm_storage_account.storage_account.name
+#   storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
+
+
+#   site_config {
+#     application_stack {
+#       dotnet_version = "6"
+#     }
+#   }
+
+# }
+
+# resource "azurerm_function_app_function" "function_app_function_staging" {
+#   name            = "function-stage-${var.app_name}"
+#   function_app_id = azurerm_windows_function_app_slot.function_app_function_slot.id
+#   language        = "CSharp"
+
+
+#   file {
+#     name    = "run_stage.csx"
+#     content = file("../azure-simple-app-service-function/function/run_stage.csx")
+#   }
+
+#   test_data = jsonencode({
+#     "name" = "Foo"
+#   })
+
+#   config_json = jsonencode({
+#     "bindings" = [
+#       {
+#         "authLevel" = "function"
+#         "direction" = "in"
+#         "methods" = [
+#           "get",
+#           "post",
+#         ]
+#         "name" = "req"
+#         "type" = "httpTrigger"
+#       },
+#       {
+#         "direction" = "out"
+#         "name"      = "$return"
+#         "type"      = "http"
+#       },
+#     ]
+#   })
+# }
